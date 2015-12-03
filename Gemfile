234df98e83d4a9b5f0c2a64d519d1d32c1d32544
Ines Duverge <inesduverge@hotmail.com>
2015-12-03 01:36:26 +0000

    create login logic

--- Gemfile
+++ Gemfile
@@ -14,6 +14,8 @@ gem 'coffee-rails', '~> 4.1.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
+gem 'bcrypt-ruby', :require => "bcrypt"
+
 source 'https://rails-assets.org' do
 	gem 'rails-assets-bootstrap'
 	gem 'rails-assets-font-awesome'
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  get "log_in" => 'sessions#new', :as => "log_in"
+  get "log_out" => 'sessions#destroy', :as => "log_out"
+  get "sign_up" => 'users#new', :as => "sign_up"
+  root :to => "users#new"
+  resources :users
+  resources :sessions
+
   resources :todos, only: [:index, :create]
 end
