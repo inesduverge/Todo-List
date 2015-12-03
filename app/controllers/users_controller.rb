3ddb64b3a4149c8b34a1010e82819141e03a196d
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-12-03 22:42:59 +0000

    create user using sql

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,48 @@
 class UsersController < ApplicationController
+
+  def encrypt_password(password)
+    if password.present?
+      password_salt = BCrypt::Engine.generate_salt
+      password_hash = BCrypt::Engine.hash_secret(password, password_salt)
+    end
+    return { password_salt: password_salt, password_hash: password_hash }
+  end
+
   def new
   	@user = User.new
   end
 
   def create
-  	@user = User.new(user_params)
-  	if @user.save
-  		redirect_to root_url, :notice => "Signed up!"
-  	else
-  		render "new"
-  	end
+    # obtem a conexao com a base de dados
+    sql_connection = ActiveRecord::Base.connection
+
+    # encrypts the password
+    passwords = encrypt_password(user_params[:password])
+
+    # the insert sql command
+    insert_command = "INSERT INTO users (email, password_salt, password_hash, created_at, updated_at) values ('#{user_params[:email]}', '#{passwords[:password_salt]}', '#{passwords[:password_hash]}', '#{Time.now}', '#{Time.now}')"
+
+    # the search user sql command
+    select_command = "SELECT * FROM users WHERE email = '#{user_params[:email]}'"
+
+    if !User.find_by_sql(select_command).first.nil?
+      flash[:alert] = "Username already in use"
+      redirect_to sign_up_path
+    else
+      sql_connection.execute(insert_command)
+      @user = User.find_by_sql(select_command).first
+      if !@user.nil?
+  		  redirect_to root_url, :notice => "Signed up!"
+  	  else
+        flash[:alert] = 'Could not create user'
+  		  render "new"
+  	  end
+    end
   end
 
   private 
 
   def user_params
-  	params.require(:user).permit(:email, :password, :password_confirmation)
+  	params.require(:user).permit(:email, :password)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ActiveRecord::Base
 	attr_accessor :password
-	before_save :encrypt_password
 
 	validates_confirmation_of :password
 	validates_presence_of :password, :on => :create
@@ -8,16 +7,12 @@ class User < ActiveRecord::Base
 	validates_uniqueness_of :email
 
 	def self.authenticate(email, password)
-		user = User.find_by_email(email)
+    select_command = "SELECT * FROM users WHERE email = '#{email}'"
+    user = User.find_by_sql(select_command).first
+
+    user.password_salt
 		if user && user.password_hash == BCrypt::Engine.hash_secret(password, user.password_salt)
 			user
-		end
-	end
-
-	def encrypt_password
-		if password.present?
-			self.password_salt = BCrypt::Engine.generate_salt
-			self.password_hash = BCrypt::Engine.hash_secret(password, password_salt)
 		end
 	end
 end
