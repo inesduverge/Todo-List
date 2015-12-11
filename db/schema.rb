1f707b02b1272c77c79decc9920796c89f9a0e47
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-12-11 14:16:05 +0000

    mais um commit da adriana

--- db/schema.rb
+++ db/schema.rb
@@ -32,8 +32,8 @@ ActiveRecord::Schema.define(version: 20151210152742) do
   end
 
   create_table "notes", force: :cascade do |t|
-    t.string   "title",       null: false
     t.integer  "tab_id"
+    t.string   "title",       null: false
     t.text     "description", null: false
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
