7225b25c8439124e8f0d207b19536ede4907087b
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-12-11 18:08:21 +0000

    Styles added

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,10 +1,3 @@
 //Place all the behaviors and hooks related to the matching controller here.
 //All this logic will automatically be available in application.js.
 //You can use CoffeeScript in this file: http://coffeescript.org/
-
-
-document.getElementById("new").addEventListener("click", aparece);
-
-function aparece() {
-        alert("pooooooop");
-    };
