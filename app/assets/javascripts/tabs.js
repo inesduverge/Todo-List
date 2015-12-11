0616d2baa46c01884a3aa7cc65a99aced51c8a80
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-12-11 13:03:14 +0000

    adriana made some changes to the code

---
+++ app/assets/javascripts/tabs.js
@@ -0,0 +1,10 @@
+//Place all the behaviors and hooks related to the matching controller here.
+//All this logic will automatically be available in application.js.
+//You can use CoffeeScript in this file: http://coffeescript.org/
+
+var ct = document.getElementById("new");
+ct.addEventListener("click", aparece);
+
+function aparece() {
+        alert("pooooooop");
+    };
