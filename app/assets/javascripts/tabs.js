40d07bdaeca363f3888174a3b90adb8fe6b60fc8
tiagonbotelho <tiagonbotelho@hotmail.com>
2015-12-13 13:52:12 +0000

    mais coisas

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,3 +1,9 @@
 //Place all the behaviors and hooks related to the matching controller here.
 //All this logic will automatically be available in application.js.
 //You can use CoffeeScript in this file: http://coffeescript.org/
+/*$(document).ready(
+    function edita(clas) {
+        console.log(clas);
+        $(clas).css( "display", "block" );
+        
+    });*/
